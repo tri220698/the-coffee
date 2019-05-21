2e1851e7cc3c957c26a7024a757e3a5f51c5fdb2
Mai Trí <maitri221998@gmail.com>
2019-05-21 13:43:29 +0700

    make it better

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('/*', (req,res) => {
   res.sendFile(path.join(__dirname, '/dist/exam/index.html'));
 });
 
-app.listen(process.env.PORT || 8080, () => {
+app.listen(process.env.PORT || 4200, () => {
   console.log('Server started');
 })
